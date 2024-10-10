f88b6dc81542549a8b6db6c8d7cc964d304ecd57
Cristian Perez <cperezlecaros@gmail.com>
2024-10-10 09:12:30 -0700

    Style loader

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -7,12 +7,13 @@ function AppLayout() {
   //global navigation. idle, loading or submitting
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
+
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header />
       <div className="overflow-scroll">
-        <main className="mx-auto max-w-3xl overflow-scroll">
+        <main className="mx-auto max-w-3xl overflow-auto">
           {/*Outlet renders child component */}
           <Outlet />
         </main>
