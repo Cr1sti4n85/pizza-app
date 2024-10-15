46c8cb0048fbae67a560892fffe0c056c4bcbba3
Cristian Perez <cperezlecaros@gmail.com>
2024-10-15 10:42:16 -0700

    Implement EmptyCart component and build Cart page

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -43,6 +43,8 @@ export const {
 
 export default cartSlice.reducer;
 
+export const getCart = (state) => state.cart.cart;
+
 export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, curItem) => sum + curItem.quantity, 0);
 
